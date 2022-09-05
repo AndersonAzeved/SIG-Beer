39b508ddc7efaa32a15cf2e04e618a4343803b85
Ericleisonn <111910474+Ericleisonn@users.noreply.github.com>
2022-09-05 20:39:07 -0300

    Adição de módulos lixeiras e relatórios
    Adição de módulos lixeiras e relatórios no programa

--- main.c
+++ main.c
@@ -3,18 +3,17 @@
 ///                 Centro de Ensino Superior do Seridó                     ///
 ///               Departamento de Computação e Tecnologia                   ///
 ///                  Disciplina DCT1106 -- Programação                      ///
-///               	SIG - Beer: Assinatura de Cervejas 		                ///
+///               	SIG - Beer: Assinatura de Cervejas ///
 ///    Developed by @andersonazeved and @ericleisonn -- since Aug, 2022     ///
 ///////////////////////////////////////////////////////////////////////////////
 ///                                Semana 1                                 ///
 ///////////////////////////////////////////////////////////////////////////////
 
-
-//Importacao de bibliotecas
+// Importacao de bibliotecas
+#include "biblioteca.h" //Importacao da biblioteca criada, e feita entre ""
+#include <locale.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <locale.h>
-#include "biblioteca.h" //Importacao da biblioteca criada, e feita entre ""
 
 int tela_principal(void);
 void tela_equipe(void);
@@ -22,38 +21,33 @@ void tela_sobre(void);
 
 int op;
 
-int main(void){
-	setlocale(LC_ALL,"Portuguese");
-	
-	do{
-		op = tela_principal();
+int main(void) {
+  setlocale(LC_ALL, "Portuguese");
 
-		if(op == 1){
-			printf("Módulo Cervejas");
+  do {
+    op = tela_principal();
+
+    if (op == 1) {
+      printf("Módulo Cervejas");
       tela_cervejas();
-		}
-		else if(op == 2){
-			printf("Módulo Assinaturas");
-		}
-		else if(op == 3){
-			printf("Módulo Fornecedores");
-		}
-		else if(op == 4){
-			printf("Módulo relatórios");
-			tela_equipe();
-		}
-		else if(op == 5){
-			printf("Equipe");
-			tela_sobre(); 
-		}
-    else if (op==6){
+    } else if (op == 2) {
+      printf("Módulo Assinaturas");
+    } else if (op == 3) {
+      printf("Módulo Fornecedores");
+    } else if (op == 4) {
+      printf("Módulo relatórios");
+      tela_relatorio();
+    } else if (op == 5) {
+      printf("Equipe");
+      tela_equipe();
+    } else if (op == 6) {
       printf("Sobre");
-    }
-    else if(op==7){
+      tela_sobre();
+    } else if (op == 7) {
       printf("Lixeira");
-      
+      tela_lixeira();
     }
 
-	}while(op != 0);	
-	return 0;
+  } while (op != 0);
+  return 0;
 }
