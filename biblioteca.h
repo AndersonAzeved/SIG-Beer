ce2f028784f532a42c5aee5fb17f853cfd5704c4
andersonazeved <andersonsilva14.2017@gmail.com>
2022-09-19 09:31:48 -0300

    Adição de comentários

--- biblioteca.h
+++ biblioteca.h
@@ -1,17 +1,17 @@
 // Assinaturas das Funções
-int tela_principal(void);
-void tela_equipe(void);
-void tela_sobre(void);
-int tela_relatorio(void);
-int tela_lixeira(void);
+int tela_principal(void); //Menu Principal 
+void tela_equipe(void); //Menu Principal
+void tela_sobre(void); //Menu Principal
+int tela_relatorio(void); 
+int tela_lixeira(void); 
 int tela_cervejas(void);
 void tela_opcao_invalida(void);
 int tela_assinaturas(void);
 int tela_fornecedores(void);
 void tela_encerramento(void);
-void cadastrar_assinatura(void);
-void atualizar_assinatura(void);
-void buscar_assinatura(void);
+void cadastrar_assinatura(void); //Módulo Assinatura
+void atualizar_assinatura(void); //Módulo Assinatura
+void buscar_assinatura(void); //Módulo Assinatura
 
 // Fim das assinaturas 
 
