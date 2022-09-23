e99cf0023cba9138da493ae48f9b4cb46b44a925
andersonazeved <andersonsilva14.2017@gmail.com>
2022-09-23 09:49:56 -0300

    Assinaturas das funções da tela principal

--- biblioteca.h
+++ biblioteca.h
@@ -1,22 +1,27 @@
 // Assinaturas das Funções
-int tela_principal(void); //Menu Principal 
-void tela_equipe(void); //Menu Principal
-void tela_sobre(void); //Menu Principal
+
+//int tela_principal(void); //Menu Principal 
+//void tela_equipe(void); //Menu Principal
+//void tela_sobre(void); //Menu Principal
 int tela_relatorio(void); 
-int tela_lixeira(void); 
-int tela_cervejas(void);
-void tela_opcao_invalida(void);
-int tela_assinaturas(void);
+//int tela_lixeira(void); 
+// void tela_opcao_invalida(void);
 // int tela_fornecedores(void);
-void tela_encerramento(void);
+
+//void tela_encerramento(void);
+
+int tela_assinaturas(void);
 void cadastrar_assinatura(void); //Módulo Assinatura
 void atualizar_assinatura(void); //Módulo Assinatura
 void buscar_assinatura(void); //Módulo Assinatura
+
+int tela_cervejas(void);
 void cadastrar_cerveja(void);
 void atualizar_cerveja(void);
 void apagar_cerveja(void);
 void recuperar_cerveja(void);
 void buscar_cerveja(void);
+
 // void cadastrar_fornecedor(void);
 // void atualizar_fornecedor(void);
 // void apagar_fornecedor(void);
---
+++ bibliotecas/principal/principal.h
@@ -0,0 +1,8 @@
+// Assinaturas das Funções da tela principal
+
+int tela_principal(void);
+void tela_equipe(void);
+void tela_sobre(void);
+int tela_lixeira(void);
+void tela_opcao_invalida(void);
+void tela_encerramento(void);
