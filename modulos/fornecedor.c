d2be5b9e1ca4e902f860272f84603e1d31bb6e53
andersonazeved <andersonsilva14.2017@gmail.com>
2022-11-10 12:33:28 -0300

    atualização

--- modulos/fornecedor.c
+++ modulos/fornecedor.c
@@ -2,8 +2,10 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "fornecedor.h"
 #include "biblioteca.h"
+#include "principal.h"
 
 typedef struct fornecedor Fornecedor;
 Fornecedor forne;
@@ -139,32 +141,69 @@ void buscar_fornecedor(void){
 
 char tela_fornecedores(void){
   char op[11];
-  system("clear||cls");
-  printf("\n"
-  "//////////////////////////////////////////////////////////////////////////////\n"
-  "///                                                                        ///\n"
-  "///             Universidade Federal do Rio Grande do Norte                ///\n"
-  "///                 Centro de Ensino Superior do Seridó                    ///\n"
-  "///               Departamento de Computação e Tecnologia                  ///\n"
-  "///                  Disciplina DCT1106 -- Programação                     ///\n"
-  "///                SIG - Beer: Assinatura de Cervejas                      ///\n"
-  "///    Developed by @andersonazeved and @ericleisonn -- since Aug,2022     ///\n"
-  "///                                                                        ///\n"
-  "//////////////////////////////////////////////////////////////////////////////\n"
-  "///                                                                        ///\n"
-  "///         = = = = Sistema de assinatura de cervejas = = = =              ///\n"
-  "///               = = = = Módulo de Fornecedores = = = =                   ///\n"
-  "///                                                                        ///\n"
-  "///             1. Cadastrar Fornecedores                                  ///\n"
-  "///             2. Atualizar Fornecedores                                  ///\n"
-  "///             3. Deletar Fornecedores                                    ///\n"
-  "///             4. Recuperar Fornecedores                                  ///\n"
-  "///             5. Pesquisar Fornecedores                                  ///\n"
-  "///             0. Voltar                                                  ///\n"
-  "///                                                                        ///\n"
-  "//////////////////////////////////////////////////////////////////////////////\n");
-  printf("Informe a opção: "); 
-  scanf("%s", op); 
+  int ok = 0;
+  do{
+    system("clear||cls");
+    printf("\n"
+    "//////////////////////////////////////////////////////////////////////////////\n"
+    "///                                                                        ///\n"
+    "///             Universidade Federal do Rio Grande do Norte                ///\n"
+    "///                 Centro de Ensino Superior do Seridó                    ///\n"
+    "///               Departamento de Computação e Tecnologia                  ///\n"
+    "///                  Disciplina DCT1106 -- Programação                     ///\n"
+    "///                SIG - Beer: Assinatura de Cervejas                      ///\n"
+    "///    Developed by @andersonazeved and @ericleisonn -- since Aug,2022     ///\n"
+    "///                                                                        ///\n"
+    "//////////////////////////////////////////////////////////////////////////////\n"
+    "///                                                                        ///\n"
+    "///         = = = = Sistema de assinatura de cervejas = = = =              ///\n"
+    "///               = = = = Módulo de Fornecedores = = = =                   ///\n"
+    "///                                                                        ///\n"
+    "///             1. Cadastrar Fornecedores                                  ///\n"
+    "///             2. Atualizar Fornecedores                                  ///\n"
+    "///             3. Deletar Fornecedores                                    ///\n"
+    "///             4. Recuperar Fornecedores                                  ///\n"
+    "///             5. Pesquisar Fornecedores                                  ///\n"
+    "///             0. Voltar                                                  ///\n"
+    "///                                                                        ///\n"
+    "//////////////////////////////////////////////////////////////////////////////\n");
+    printf("Informe a opção: "); 
+    fgets(op, 10, stdin);
+    remove_enter(op);
+    if((strlen(op) == 1) && (op[0] >= '0' && op[0] <= '5')){
+      ok = 1;
+    }else{
+      system("clear||cls");
+      tela_opcao_invalida();
+      system("clear||cls");
+      printf("\n"
+      "//////////////////////////////////////////////////////////////////////////////\n"
+      "///                                                                        ///\n"
+      "///             Universidade Federal do Rio Grande do Norte                ///\n"
+      "///                 Centro de Ensino Superior do Seridó                    ///\n"
+      "///               Departamento de Computação e Tecnologia                  ///\n"
+      "///                  Disciplina DCT1106 -- Programação                     ///\n"
+      "///                SIG - Beer: Assinatura de Cervejas                      ///\n"
+      "///    Developed by @andersonazeved and @ericleisonn -- since Aug,2022     ///\n"
+      "///                                                                        ///\n"
+      "//////////////////////////////////////////////////////////////////////////////\n"
+      "///                                                                        ///\n"
+      "///         = = = = Sistema de assinatura de cervejas = = = =              ///\n"
+      "///               = = = = Módulo de Fornecedores = = = =                   ///\n"
+      "///                                                                        ///\n"
+      "///             1. Cadastrar Fornecedores                                  ///\n"
+      "///             2. Atualizar Fornecedores                                  ///\n"
+      "///             3. Deletar Fornecedores                                    ///\n"
+      "///             4. Recuperar Fornecedores                                  ///\n"
+      "///             5. Pesquisar Fornecedores                                  ///\n"
+      "///             0. Voltar                                                  ///\n"
+      "///                                                                        ///\n"
+      "//////////////////////////////////////////////////////////////////////////////\n");
+      printf("Informe a opção: "); 
+      fgets(op, 10, stdin);
+      remove_enter(op);
+    }    
+  }while(!ok);
   getchar();
   printf("\n");
   return op[0];
